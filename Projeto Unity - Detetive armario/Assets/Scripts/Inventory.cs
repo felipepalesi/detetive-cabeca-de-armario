b8d6aa6b361fa81101d38b3cefcdefd8de2eeef9
felipepalesi <61666026+felipepalesi@users.noreply.github.com>
2020-03-01 13:01:43 -0300

    Criando o repo

---
+++ Projeto Unity - Detetive armario/Assets/Scripts/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public GameObject[] inventory = new GameObject[10];
+
+    public void AddItem(GameObject item)
+    {
+        bool itemAdded = false;
+
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory [i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + "entrou");
+                itemAdded = true;
+                item.SendMessage("DoInteraction");
+                break;
+            }
+        }
+        //inventario cheio
+        if (!itemAdded)
+        {
+            Debug.Log("inventario cheio");
+        }
+
+    }
+
+    public bool FindItem(GameObject item)
+    {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory[i] == item)
+            {
+                //item foi encontrado
+                return true;
+            }
+        }
+
+        //item não foi encontrado
+        return false;
+    }
+}
---
+++ Projeto Unity - Detetive armario/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(feetPos.position, checkRadius);
+    }
+}
